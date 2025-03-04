ba8c10152bdc6580237553706284f5a10bef5741
SarahWagner <sarah.wagner@inwt-statistics.de>
2025-03-04 15:12:33 +0100

    updates for mongo

--- R/03_db_creds.R
+++ R/03_db_creds.R
@@ -1,49 +1,44 @@
-#' Read Database Credentials from .Renviron
+#' Read MongoDB Credentials from .Renviron
 #'
-#' This function reads database credentials from environment variables defined in the
-#' .Renviron file and returns a credentials object.
+#' This function reads MongoDB database credentials from environment variables defined in the
+#' .Renviron file and returns a mongo connection object.
 #'
 #' The .Renviron file should contain the following environment variables:
 #' \itemize{
-#'   \item \code{DB_USER}: The username for the database.
-#'   \item \code{DB_PASSWORD}: The password for the database.
-#'   \item \code{DB_HOST}: The host address of the database.
-#'   \item \code{DB_NAME}: The name of the database.
-#'   \item \code{DB_PORT}: The port number of the database.
+#'   \item \code{DB_USER}: The username for the MongoDB database.
+#'   \item \code{DB_PASSWORD}: The password for the MongoDB database, please use percent encoding
+#'     here (https://developer.mozilla.org/en-US/docs/Glossary/Percent-encoding).
+#'   \item \code{DB_HOST}: The host address of the MongoDB server.
+#'   \item \code{DB_NAME}: The name of the MongoDB database.
+#'   \item \code{DB_PORT}: The port number of the MongoDB server.
 #' }
 #'
-#' @return A list containing the database credentials:
-#' \itemize{
-#'   \item \code{user}: The username for the database.
-#'   \item \code{password}: The password for the database.
-#'   \item \code{host}: The host address of the database.
-#'   \item \code{dbname}: The name of the database.
-#'   \item \code{port}: The port number of the database.
-#' }
+#' @return A mongo connection object that can be used to interact with the MongoDB database.
 #'
 #' @export
 credentials <- function() {
-  return(
-    Credentials(
-      drv = RMySQL::MySQL,
-      user = Sys.getenv("DB_USER"),
-      password = Sys.getenv("DB_PASSWORD"),
-      dbname = Sys.getenv("DB_NAME"),
-      host = Sys.getenv("DB_HOST"),
-      port = as.numeric(Sys.getenv("DB_PORT"))
-    )
+  URI <- sprintf(
+    "mongodb://%s:%s@%s:%s/%s",
+    Sys.getenv("DB_USER"),
+    Sys.getenv("DB_PASSWORD"),
+    Sys.getenv("DB_HOST"),
+    Sys.getenv("DB_PORT"),
+    Sys.getenv("DB_NAME")
   )
+
+  return(mongo(url = URI))
 }
 
-#' Update Database with New RSS Feed Data
+
+#' Update MongoDB with New RSS Feed Data
 #'
-#' This function updates the database with new RSS feed data from a list of tibbles.
+#' This function updates a MongoDB collection with new RSS feed data from a tibble.
 #' For each tibble, it compares the `feed_last_build_date` with the corresponding
-#' date in the database and updates the database if the tibble contains newer data.
+#' date in the database and updates the collection if the tibble contains newer data.
 #'
 #' @param tibble A tibble containing RSS feed data with unique `source_id`
 #'   and `feed_last_build_date`.
-#' @param config (list) configuration file with column mapping and rss field names.
+#' @param config (list) Configuration file with column mapping and RSS field names.
 #' @export
 update_database <- function(tibble, config) {
   mapping <- read_field_mapping(config)
@@ -53,21 +48,18 @@ update_database <- function(tibble, config) {
   timestamp_feed_updated_rss <- mapping["timestamp_feed_updated", ]$rss
   max_timestamp_feed_updated_rss <- max(tibble[[timestamp_feed_updated_rss]])
 
-  query <- sprintf(
-    "SELECT MAX(%s) AS max_date FROM feed_text WHERE source_id = %s;",
-    timestamp_feed_updated_db,
-    source_id
-  )
   con <- credentials()
-  max_timestamp_feed_updated_db <- sendQuery(con, query)$max_date
 
-  if (is.na(max_timestamp_feed_updated_db) ||
-      max_timestamp_feed_updated_rss > max_timestamp_feed_updated_db) {
+  query <- sprintf('{"source_id": %s}', source_id)
+  max_timestamp_feed_updated_db <- con$find(query, fields = sprintf('{"%s": 1, "_id": 0}', timestamp_feed_updated_db))
+
+  if (nrow(max_timestamp_feed_updated_db) == 0 ||
+      max_timestamp_feed_updated_rss > max(max_timestamp_feed_updated_db[[timestamp_feed_updated_db]], na.rm = TRUE)) {
     logging("Sending data for source_id %s.", source_id)
     rename_vector <- setNames(as.character(mapping$rss), mapping$db)
-    tibble <- tibble %>%
-      rename(!!!rename_vector)
-    sendData(con, tibble, table = "feed_text", mode = "replace")
+    tibble <- tibble %>% rename(!!!rename_vector)
+
+    con$insert(tibble)
   } else {
     logging("Nothing to update for source_id %s.", source_id)
   }
